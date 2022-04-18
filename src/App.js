47e89f91d0cadafee15d2a24c33ba92709c98efa
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-04-18 18:54:14 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     // requisito 4 pasei o estado inicial das minhas variaveis
     this.state = {
       cardName: '',
@@ -15,8 +15,10 @@ class App extends React.Component {
       cardImage: '',
       cardRare: 'normal',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.verificar = this.verificar.bind(this);
   }
 
   // requisito 4 passar o evento de onchange a funcao oninputchange
@@ -24,13 +26,40 @@ class App extends React.Component {
     if (event.target.name === 'cardTrunfo') return this.setState({ cardTrunfo: true });
     this.setState({
       [event.target.name]: event.target.value,
+    }, () => {
+      this.verificar();
     });
+  }
+
+  verificar = () => {
+    const { cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 } = this.state;
+    const numMax = 210;
+    const numUnic = 90;
+    const sum = number(cardAttr1) + number(cardAttr2) + number(cardAttr3);
+    if (cardName.length > o && cardDescription.length > 0
+    && cardImage.length > 0
+    && cardRare.length > 0
+    && sum <= numMax
+    && cardAttr1 >= 0 && cardAttr1 <= numUnic
+    && cardAttr2 >= 0 && cardAttr2 <= numUnic
+    && cardAttr3 >= 0 && cardAttr3 <= numUnic) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   }
 
   render() {
     // requisito 4 desestruturei os estados para usar em cada componente. e o valor das props sao os estados iniciais.asim ele fica dinamico
     const { cardName, cardDescription, cardAttr1,
-      cardAttr2, cardImage, cardRare, cardTrunfo, cardAttr3 } = this.state;
+      cardAttr2, cardImage, cardRare, cardTrunfo,
+      cardAttr3, isSaveButtonDisabled } = this.state;
     return (
       <div>
         <h1>Adicionar Nova Carta</h1>
@@ -44,6 +73,8 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ () => console.log('ola') }
         />
 
         <Card
